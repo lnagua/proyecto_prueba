be9564a2fca249338471bf5acefc3513b37d842a
lnagua <lnagua@ing.uc3m.es>
2026-02-26 13:22:33 +0100

    segundo comentario

--- point.cpp
+++ point.cpp
@@ -4,20 +4,3 @@
 #include "point.h"
 #include <iostream>
 
-
-int Point::getX() {
-    return _x;
-}
-
-int Point::getY() {
-    return _y;
-}
-
-void Point::set(int x, int y) {
-    _x = x;
-    _y = y;
-}
-
-void Point::display() {
-    std::cout << "Point(" << _x << ", " << _y << ")" << std::endl;
-}
